aa9e6c7a74fb56b3a0e0f885468f05d4841c4179
Rohan Dalvi <rohan_dalvi@icloud.com>
2019-10-25 23:48:38 -0700

    Added dynamodb annotations

--- src/main/java/mapper/Profile.java
+++ src/main/java/mapper/Profile.java
@@ -1,5 +1,6 @@
 package mapper;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.AllArgsConstructor;
@@ -8,22 +9,56 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
-@Data
 @DynamoDBTable(tableName = "Profile")
 public class Profile {
     private String id = UUID.randomUUID().toString();
-    private final String userId;
-    private final String email;
-    private final String firstName;
-    private final String lastName;
+    private String userId;
+    private String email;
+    private String firstName;
+    private String lastName;
 
-    @DynamoDBHashKey
+    @DynamoDBHashKey(attributeName = "userId")
     public String getUserId() {
         return userId;
     }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBHashKey(attributeName = "id")
     public String getId() {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDBAttribute(attributeName = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @DynamoDBAttribute(attributeName = "firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @DynamoDBAttribute(attributeName = "lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
